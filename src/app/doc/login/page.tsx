03155d42be56fe429011f3a534151251bd413e07
Daniil Shcherbakov <dshcherbakov@seamgroup.com>
2023-05-24 03:11:25 +0200

    fix labels

--- src/app/doc/login/page.tsx
+++ src/app/doc/login/page.tsx
@@ -9,7 +9,7 @@ type Props = {}
 const LoginPage = (props: Props) => {
   return (
     <div className={'flex flex-col h-screen py-0 sm:py-44 items-center justify-center bg-animation-gradient'}>
-      <h1 className='text-3xl text-white mb-10 font-light'>Панель доктора</h1>
+      <h1 className='text-3xl text-white mb-10 font-light'>Панель лікаря</h1>
       <LoginForm forDoc /></div>
   )
 }
--- src/app/doc/register/page.tsx
+++ src/app/doc/register/page.tsx
@@ -6,7 +6,7 @@ type Props = {}
 const RegisterPage = (props: Props) => {
   return (
     <div className='flex flex-col h-screen py-0 sm:py-44 items-center justify-center bg-animation-gradient'>
-        <h1 className='text-3xl absolute top-20 left-1/2 -translate-x-1/2  text-white mb-10 font-light'>Панель доктора</h1><RegisterForm forDoc/></div>
+        <h1 className='text-3xl absolute top-20 left-1/2 -translate-x-1/2  text-white mb-10 font-light'>Панель лікаря</h1><RegisterForm forDoc/></div>
   )
 }
 
